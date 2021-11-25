e7bfacc246ff255f79cfc69682e8712588d99c2e
kadzat <kadzat.leusmana14@gmail.com>
2021-11-25 11:30:25 +0700

    konfigurasi file setting.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contents.apps.ContentsConfig',
-    'contents'
+    'contents.apps.ContentsConfig'
 ]
 
 MIDDLEWARE = [
