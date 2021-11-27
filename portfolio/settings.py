c083831d69a516e788655a778f85303d020b4783
kadzat <kadzat.leusmana14@gmail.com>
2021-11-27 20:37:46 +0700

    menghapus sebagian file dan meng konfigurasi di file settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,9 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATIC_ROOT = ''  # for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
@@ -130,4 +129,4 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
